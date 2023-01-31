6f74e0480a79a70fd4c34de8581bc0d4d7ceb9da
ajohnson5 <99501368+ajohnson5@users.noreply.github.com>
2023-01-31 18:37:33 +0000

    Restructured DAG and added season folder structure
    Removed the two assets, players_to_join and team_to_join, as I did not need these df persisted in storage.

--- pipeline.py
+++ pipeline.py
@@ -18,9 +18,8 @@ from fpl_api_getter import (
     team_getter
 )
 
-
+SEASON = '2022'
 gameweek_list = [str(gw) for gw in range(1,39)]
-
 gameweek_partitions = StaticPartitionsDefinition(gameweek_list)
 
 @asset(io_manager_key='gcs_io_manager')
@@ -32,28 +31,16 @@ def all_team_stats() -> pd.DataFrame:
 
 
 @asset(io_manager_key='gcs_io_manager')
-def teams_to_join(context, all_team_stats) -> pd.DataFrame:
-    return all_team_stats[['team_id','team_name']]
-
-
-
-
-@asset(io_manager_key='gcs_io_manager')
-def all_players_stats() -> pd.DataFrame:
+def all_player_stats() -> pd.DataFrame:
 
     df = pd.DataFrame(player_getter())
     df.rename(columns = {'team':'team_id'}, inplace = True)
 
     return df
 
-@asset(io_manager_key='gcs_io_manager')
-def players_to_join(all_players_stats) -> pd.DataFrame:
-    return all_players_stats[['id','first_name','second_name','team_id']]
-
-    
 
 @asset(partitions_def=gameweek_partitions,io_manager_key='gcs_io_manager')
-def gw_summary(context, players_to_join, teams_to_join) -> pd.DataFrame:
+def gw_summary(context, all_player_stats, all_team_stats) -> pd.DataFrame:
     '''
     Summary - This asset returns a pd.DataFrame containing all player stats for a specified gameweek
 
@@ -62,8 +49,8 @@ def gw_summary(context, players_to_join, teams_to_join) -> pd.DataFrame:
     '''
     df = pd.DataFrame(gw_stats_getter(context.partition_key))
     #Merge the gw dataframe to the team and player_name dataframes 
-    df = df.merge(players_to_join, how = 'left', on ='id' )
-    df = df.merge(teams_to_join, how = 'left',on ='team_id')
+    df = df.merge(all_player_stats[['id','first_name','second_name','team_id']], how = 'left', on ='id' )
+    df = df.merge(all_team_stats[['team_id','team_name']], how = 'left',on ='team_id')
     #Add a column which indicates the gameweek the dataframe refers too.
     df['gameweek'] = int(context.partition_key)
 
@@ -71,12 +58,12 @@ def gw_summary(context, players_to_join, teams_to_join) -> pd.DataFrame:
 
 
 @asset(non_argument_deps = {'gw_summary'},required_resource_keys={"bq_res"})
-def combined_gw_summary(context) -> None:
+def bigquery_gw_summary(context) -> None:
 
     # Big query configuring variables
     project_ID = ''
-    bucket_name =''
-    dataset_name =''
+    bucket_name = ''
+    dataset_name = ''
     
     #Define job config for BQ, note we overwrite the existing table
     job_config = bigquery.LoadJobConfig(
@@ -85,10 +72,33 @@ def combined_gw_summary(context) -> None:
     )
 
     #uri to load all of the parquet files in the gw_summary directory
-    batch_uri = f'gs://{bucket_name}/gw_summary/*.parquet'
+    batch_uri = f'gs://{bucket_name}/{SEASON}/gw_summary/*.parquet'
 
     load_job = context.resources.bq_res.load_table_from_uri(
-        batch_uri, f'{project_ID}.{dataset_name}.gw_summary', job_config=job_config
+        batch_uri, f'{project_ID}.{dataset_name}.{SEASON}_gw_summary', job_config=job_config
+    ) 
+
+    return None
+
+
+@asset(non_argument_deps = {'all_player_stats'},required_resource_keys={"bq_res"})
+def bigquery_all_player_stats(context) -> None:
+    # Big query configuring variables
+    project_ID = ''
+    bucket_name = ''
+    dataset_name = ''
+    
+    #Define job config for BQ, note we overwrite the existing table
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.PARQUET,
+        write_disposition="WRITE_TRUNCATE"
+    )
+
+    #uri to load all of the parquet files in the gw_summary directory
+    uri = f'gs://{bucket_name}/{SEASON}/all_player_stats/*.parquet'
+
+    load_job = context.resources.bq_res.load_table_from_uri(
+        uri, f'{project_ID}.{dataset_name}.{SEASON}_all_player_stats', job_config=job_config
     ) 
 
     return None
@@ -96,11 +106,10 @@ def combined_gw_summary(context) -> None:
 
 @io_manager(required_resource_keys={'gcs'})
 def gcs_parquet_io_manager(init_context):
-    return GCSParquetIOManager('')
+    return GCSParquetIOManager('fpl_dev_bucket', season = SEASON)
+
 
 defos = Definitions(
-    assets=[gw_summary, combined_gw_summary,all_players_stats, players_to_join,
-    all_team_stats,teams_to_join],
+    assets=[gw_summary, bigquery_gw_summary,all_team_stats,all_player_stats,bigquery_all_player_stats],
     resources={"gcs_io_manager": gcs_parquet_io_manager,"gcs": gcs_resource,
      "bq_res":bigquery_resource})
-
