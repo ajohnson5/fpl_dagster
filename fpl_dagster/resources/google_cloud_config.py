9f8bacdae0e550e9b43874689b8bc24634d6ded0
ajohnson5 <agjohnson1111@gmail.com>
2023-03-13 19:22:47 +0000

    Changed dict functions to dict literals {}

--- fpl_dagster/resources/google_cloud_config.py
+++ fpl_dagster/resources/google_cloud_config.py
@@ -3,4 +3,4 @@ from dagster import resource, StringSource
 #Resource to pass google cloud credentials to assets and io_manager
 @resource(config_schema={"project_bucket":StringSource, "project_dataset":StringSource})
 def google_cloud_config(init_context):
-    return dict(bucket = init_context.resource_config['project_bucket'], dataset = init_context.resource_config['project_dataset'])
+    return {'bucket':init_context.resource_config['project_bucket'], 'dataset':init_context.resource_config['project_dataset']}
